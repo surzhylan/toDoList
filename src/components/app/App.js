8f86c9f69c9b68d51e2b00b53fb31304cea00994
Essenbay Assel <esenbay.asel@mail.ru>
2023-02-14 22:00:57 +0600

    Merge to main

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,12 +3,13 @@ import TodoList from "../todo-list-panel/TodoList";
 import styles from "./App.module.css"
 import SearchPanel from "../search-panel/SearchPanel";
 import HabitList from "../habbits-panel/HabitList";
+import Navbar from "../navbar/navbar";
 
 
 const App = () => {
     return (
         <div className={styles.appWrapper}>
-            <AppHeader/>
+            <Navbar/>
             <SearchPanel />
             <div className={styles.lists}>
                 <TodoList/>

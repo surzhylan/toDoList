16896c89880e4bc7ed2a26b1c70dc0f9af54e40b
Aliya Shakhuali <shakhuali03@gmail.com>
2023-02-16 13:40:14 +0600

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/components/app/App.js 	modified:   src/components/app/App.module.css 	modified:   src/components/navbar/navbar.css 	modified:   src/components/search-panel/SearchPanel.js 	modified:   src/components/search-panel/SearchPanel.module.css 	modified:   src/components/todo-list-panel/TodoList.js 	modified:   src/components/todo-list-panel/TodoList.module.css 	modified:   src/components/todo-list-panel/edit-task-modal/EditTaskModal.js 	modified:   src/components/todo-list-panel/edit-task-modal/EditTaskModal.module.css 	modified:   src/components/todo-list-panel/todo-item/ToDoListItem.js 	new file:   src/components/todo-list-panel/todo-item/ToDoListItem.module.css

--- src/components/todo-list-panel/TodoList.js
+++ src/components/todo-list-panel/TodoList.js
@@ -4,7 +4,7 @@ import AddTaskForm from "./add-task-form/AddTaskForm";
 import styles from "./TodoList.module.css"
 import {v4 as uuidv4} from 'uuid';
 
-const TodoList = ({coins, setCoins}) => {
+const TodoList = () => {
     const savedTasks = localStorage.getItem('tasks')
     const [tasks, setTasks] = useState([])
 
@@ -29,39 +29,36 @@ const TodoList = ({coins, setCoins}) => {
         }
     }
 
-    const deleteTask = (deletedId) => {
+    const onDeleteTask = (deletedId) => {
         let newTasks = tasks.filter(task => task.id !== deletedId)
         setTasks(newTasks)
     }
 
-    const changeTask = (task) => {
-        if (task) {
-            const index = tasks.findIndex((obj => obj.id === task.id))
-
-            //If isCompleted changed
-            if (task.isCompleted === tasks[index].isCompleted) {
-                if (task.isCompleted === true) setCoins(coins + task.rewardAmount)
-                else setCoins(coins - task.rewardAmount)
-            }
-
+    const onTaskChanged = (changedTask) => {
+        if (changedTask) {
+            const index = tasks.findIndex((obj => obj.id === changedTask.id))
             let newTasks = [...tasks]
-            newTasks[index] = task
+            newTasks[index] = changedTask
             setTasks(newTasks)
         }
     }
 
     return (
         <div className={styles.appWrapper}>
-            <h5 className={styles.header}>List: </h5>
-            <AddTaskForm addTask={addTask}/>
-            {tasks.map((task) => {
-                return (
-                    <div key={task.id}>
-                        <ToDoListItem task={task} onDeleteTask={deleteTask} onEditTask={changeTask}/>
-                    </div>
-                )
-            })
-            }
+            <h5 className={styles.header}>Tasks: </h5>
+            <div className={styles.todolist}>
+                <AddTaskForm addTask={addTask}/>
+                <div className={styles.todoTask}>
+                    {tasks.map((task) => {
+                        return (
+                            <div key={task.id}>
+                                <ToDoListItem task={task} onDeleteTask={onDeleteTask} onEditTask={onTaskChanged}/>
+                            </div>
+                        )
+                    })
+                    }
+                </div>
+            </div>
         </div>
     )
 }

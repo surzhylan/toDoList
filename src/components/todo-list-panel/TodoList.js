e675964e0916244e5f0e03c2636d06b09c84ba42
Essenbay Assel <esenbay.asel@mail.ru>
2023-02-19 18:02:51 +0600

    Filter function added

--- src/components/todo-list-panel/TodoList.js
+++ src/components/todo-list-panel/TodoList.js
@@ -1,12 +1,8 @@
 import ToDoListItem from "./todo-item/ToDoListItem";
-import {useEffect, useState} from "react";
 import AddTaskForm from "./add-task-form/AddTaskForm";
 import styles from "./TodoList.module.css"
-import {v4 as uuidv4} from 'uuid';
 
-const TodoList = ({coins, setCoins, tasks, addTask, deleteTask, changeTask, setReward}) => {
-
-
+const TodoList = ({tasks, addTask, deleteTask, changeTask, setReward}) => {
     return (
         <div className={styles.appWrapper}>
             <h5 className={styles.header}>Tasks: </h5>
